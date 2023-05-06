47718bbe80c04f567d6e7087c52a1afb6fa404ac
andregu14 <andregustavo147@gmail.com>
2023-05-06 04:09:41 -0300

    Update ambiente.js

--- aula014/ambiente.js
+++ aula014/ambiente.js
@@ -1,11 +1,12 @@
-console.log('Vai comecar....')
 /*
+console.log('Vai comecar....')
+
 var c = 1
 while (c <= 5) {
     console.log(c)
     c++
 }
-*/
+
 
 var inicio = 5
 var fim = 10
@@ -16,3 +17,10 @@ for (inicio; inicio <= fim; inicio+= passo) {
    
 }
 console.log(passo)
+*/
+
+var numero = 10
+for (var i = 1;i < 10; i++) {
+    var res = numero * i
+    console.log(res)
+}

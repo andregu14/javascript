0e497464e41d7c9baaa2232dc1630d71fff130b2
andregu14 <andregustavo147@gmail.com>
2023-05-05 23:52:41 -0300

    Create ambiente.js

---
+++ aula013/ambiente.js
@@ -0,0 +1,33 @@
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+var agora = new Date()
+var ano = agora.getFullYear()
+while (ano <= 2030) {
+    console.log(`Ano = ${ano}`)
+    ano++
+}
+
+
+
+
+/*
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/

4a21c00d8964e5896f6d32f2e145680175c9228e
andregu14 <andregustavo147@gmail.com>
2023-05-06 02:29:33 -0300

    Create ambiente.js

---
+++ aula014/ambiente.js
@@ -0,0 +1,18 @@
+console.log('Vai comecar....')
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+
+var inicio = 5
+var fim = 10
+var passo = ''
+
+
+for (inicio; inicio <= fim; inicio+= passo) {
+   
+}
+console.log(passo)

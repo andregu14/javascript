9076c9fa3697b1f7fc7994d0dbc232ca97e17d01
andregu14 <andregustavo147@gmail.com>
2023-05-06 23:50:02 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -8,21 +8,28 @@ function contar() {
     var res = document.getElementById('res')
     var p = document.createElement('p')
     p.innerHTML = ``
-    if (inicio > fim || inicio == '' || fim == '') {
+    if (inicio == 0 || fim == 0) {
         window.alert(`[ERRO] verifique os dados e tente novamente`)
-    } else if (passo == 0 || passo > fim) {
+    } else if (passo == 0) {
         alert('[ERRO]Passo invalido, sera considerado passo 1')
         passo = 1
-    } 
-    if (passo >= 1 && inicio <= fim) {
-        res.innerHTML = `Contando de ${inicio} ate ${fim}, de ${passo} em ${passo}: <br>`
-        for (inicio;inicio <= fim;inicio += passo) {
-            p.innerHTML += `${inicio}&#x1F449`
-        }
-        p.innerHTML += 'Fim &#x1F3C1'
-        res.appendChild(p)
-        
-    }
+        } if (passo >= 1 && inicio <= fim) {
+            // Contagem crescente
+            res.innerHTML = `Contando de ${inicio} ate ${fim}, de ${passo} em ${passo}: <br>`
+            for (inicio;inicio <= fim;inicio += passo) {
+                p.innerHTML += `${inicio}&#x1F449`
+            }
+            p.innerHTML += 'Fim &#x1F3C1'
+                res.appendChild(p)
+            } else {
+                // Contagem regressiva
+                res.innerHTML = `Contando de ${inicio} ate ${fim}, de ${passo} em ${passo}: <br>`
+                for (inicio;inicio >= fim;inicio -= passo) {
+                    p.innerHTML += `${inicio}&#x1F449`
+                }
+                p.innerHTML += 'Fim &#x1F3C1'
+                res.appendChild(p)
+            }
 }
         
             

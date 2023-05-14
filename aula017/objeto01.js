bd37c3e000f76475459508b88ae5c574f60e260c
andregu14 <andregustavo147@gmail.com>
2023-05-14 04:34:55 -0300

    Update objeto01.js

--- aula017/objeto01.js
+++ aula017/objeto01.js
@@ -1,9 +1,22 @@
- let amigo = {nome: 'Jose', 
- sexo: 'M',
- peso: 85.4,
- engordar(p=0){
-    console.log('Engordou')
-    this.peso += p
- } }
+//  Estudando Objetos
+ 
+ let amigo = {
+   nome: 'Jose', 
+   sexo: '',
+   peso: 85.4,
+   engordar(p=0){
+      console.log('Engordou')
+      this.peso += p
+   } }
  amigo.engordar(2)
- console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+ amigo.nome = 'Andre'
+ amigo.sexo = 'm'
+
+ if (amigo.sexo == 'M' || amigo.sexo == 'm') {
+   amigo.sexo = 'masculino'
+ } else {
+   amigo.sexo = 'feminino'
+ }
+
+ console.log(`${amigo.nome} do sexo ${amigo.sexo} pesa ${amigo.peso}Kg`)
+

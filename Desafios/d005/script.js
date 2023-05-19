0e54938283edc2dfcf39db552f5199e4f965f76d
andregu14 <andregustavo147@gmail.com>
2023-05-19 00:15:28 -0300

    Update script.js

--- Desafios/d005/script.js
+++ Desafios/d005/script.js
@@ -3,37 +3,42 @@ function adequacao(x) { // Formata os numeros para str
     return a
 }
 
-function converteMedidas() { // Recebe e converte os valores do input do usuario
+function converteMedidas() {
     let res = document.getElementById('res')
     let res_calculo = document.getElementById('res_calculo')
-    p = document.createElement('p')
-    p.setAttribute('id', 'num')
+    let table = res_calculo.querySelector('table')
 
-    let existing_p = res_calculo.querySelector('p')
-    if (existing_p) {
-        res_calculo.removeChild(p)
+    // If table element does not exist, create it
+    if (!table) {
+        table = document.createElement('table')
+        res_calculo.appendChild(table)
+    } else {
+        // If table element exists, clear its contents
+        table.innerHTML = ''
     }
-    
-    
 
     distancia = window.prompt('Digite uma distancia em metros (m)')
     while (distancia / 1 != Number(distancia) || distancia == '' || distancia == 0) {
         distancia = window.prompt('Digite um numero valido em metros (m)')
     }
     distancia = Number(distancia)
-    
-    
 
     res.innerHTML = `<h2>A distancia de ${adequacao(distancia)} metros, corresponde a...</h2>`
-    
 
-    p.innerHTML += `${adequacao(distancia / 1000)} quilometros (Km) <br>`
-    p.innerHTML += `${adequacao(distancia / 100)} hectometros (Hm) <br>`
-    p.innerHTML += `${adequacao(distancia / 10)} decametros (Dam) <br>`
-    p.innerHTML += `${adequacao(distancia * 10)} decimetros (dm) <br>`
-    p.innerHTML += `${adequacao(distancia * 100)} centimetros (cm) <br>`
-    p.innerHTML += `${adequacao(distancia * 1000)} milimetros (mm) <br>`
-    
-    res_calculo.appendChild(p)
-    
+    let units = [
+        {unit: 'quilometros (Km)', value: distancia / 1000},
+        {unit: 'hectometros (Hm)', value: distancia / 100},
+        {unit: 'decametros (Dam)', value: distancia / 10},
+        {unit: 'decimetros (dm)', value: distancia * 10},
+        {unit: 'centimetros (cm)', value: distancia * 100},
+        {unit: 'milimetros (mm)', value: distancia * 1000}
+    ]
+
+    for (let i = 0; i < units.length; i++) {
+        let row = table.insertRow()
+        let cell1 = row.insertCell()
+        let cell2 = row.insertCell()
+        cell1.textContent = units[i].unit
+        cell2.textContent = adequacao(units[i].value)
+    }
 }

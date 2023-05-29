6d964583ee46fabe098fe8d109f2566b993486bf
André Gustavo <andregustavo147@gmail.com>
2023-05-29 18:58:25 -0300

    Update converter.js

--- Desafios/d006/converter.js
+++ Desafios/d006/converter.js
@@ -1,15 +1,29 @@
+function adequacao(x) { // Formata os numeros
+    let a = x.toLocaleString('pt-BR', {minimumFractionDigits: 1, maximumFractionDigits: 2})
+    return a
+}
+
 function converter() {
     let resultado = document.getElementById('res')
     
     // Pede input para o usuario
-    let numinput = prompt('Digite uma temperatura em ºC (Celsius)')
+    let celsius = prompt('Digite uma temperatura em ºC (Celsius)')
     
-    // Converte input para number
-    numinput = Number(numinput)
-
     // Valida input
-    while (!isFinite(numinput) || numinput === '' ) {
-        console.log('nao numero')
-        console.clear()
+    while (!isFinite(celsius) || celsius === '' ) {
+        celsius = prompt('Digite uma temperatura valida em ºC (Celsius)')
     }
+    // Converte input para number
+    celsius = Number(celsius)
+
+    // Faz a conversão
+    let fahrenheit = (celsius * (9/5)) + 32
+    let kelvin = celsius + 273.15
+
+    // Adiciona o resultado na tela
+    resultado.innerText = ''
+    resultado.innerHTML = `${adequacao(celsius)} graus celsius equivalem a:<br>
+    ${adequacao(fahrenheit)} fahrenheit<br>
+    ${adequacao(kelvin)} kelvin`
 }
+

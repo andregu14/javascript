4e5d26c1c068fcc0553a7a387f9f447ead63597a
André Gustavo <andregustavo147@gmail.com>
2023-06-01 00:17:43 -0300

    Criei animacao simples de loading, e estilo para o  botao

--- Desafios/d010/calcularBhaskara.js
+++ Desafios/d010/calcularBhaskara.js
@@ -1,13 +1,15 @@
 function calcularBhaskara () {
+    document.getElementById('loading').style.display = 'block'
     const res = document.getElementById('res')
+    res.style.display = 'none'
 
     // Funcao que verifica se um numero e valido e transforma ele em numero
     function isNumber(valor) {
-        if (isFinite(valor) || valor > 0) {
+        if (!isFinite(valor) || !valor > 0 || valor === '') {
+            return false
+        } else {
             valor = Number(valor)
             return true
-        } else {
-            return false
         }
     }
 
@@ -38,11 +40,18 @@ function calcularBhaskara () {
     let valor_delta = valor_b * valor_b - 4 * valor_a * valor_c
 
 
-    // Mostra resultado na tela
-    res.innerHTML = ''
-    res.innerHTML += `<h2>Resolvendo Bhaskara</h2>`
-    res.innerHTML += `<p>A equação atual é ${valor_a}x<sup>2</sup> + ${valor_b}x + ${valor_c} = 0</p>`
-    res.innerHTML += `<p>O cálculo realizado será Δ = ${valor_b}<sup>2</sup> - 4 . ${valor_a} . ${valor_c}</p>`
-    res.innerHTML += `<p>O valor calculado foi <span id="resultado">Δ = ${valor_delta}</span></p>`
+    setTimeout(() => {
+        res.style.display = 'flex'
+        document.getElementById('loading').style.display = 'none'
+        
+        // Mostra resultado na tela
+        res.innerHTML = ''
+        res.innerHTML += `<h2>Resolvendo Bhaskara</h2>`
+        res.innerHTML += `<p>A equação atual é ${valor_a}x<sup>2</sup> + ${valor_b}x + ${valor_c} = 0</p>`
+        res.innerHTML += `<p>O cálculo realizado será Δ = ${valor_b}<sup>2</sup> - 4 . ${valor_a} . ${valor_c}</p>`
+        res.innerHTML += `<p>O valor calculado foi <span id="resultado">Δ = ${valor_delta}</span></p>`
+    }, 1500)
+
+    
 
 }

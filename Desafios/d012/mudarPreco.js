58f2dbf7035a0c350e14438cafafe3e7c2fcb30c
André Gustavo <andregustavo147@gmail.com>
2023-06-05 22:19:12 -0300

    Create mudarPreco.js

---
+++ Desafios/d012/mudarPreco.js
@@ -0,0 +1,5 @@
+function mudarPreco() {
+    // Mostra a animacao de loading na tela
+    const res = document.getElementById('res')
+    document.getElementById('loading').style.display = 'block'
+}
